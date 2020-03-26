e937060b01c21b88611819727962c0c5e151342a
Rajesh mule <rajeshsmule@gmail.com>
2020-03-26 17:43:12 +0530

    use redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,59 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+//store -> global state
+
+//action => simple function return object
+
+const increment = () =>
+{
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () =>
+{
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+const reset = () =>
+{
+  return {
+    type: 'RESET'
+  }
+}
+
+//reducer
+
+const counter = (state = 0, action) =>
+{
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case 'RESET':
+      return state = 0;
+  }
+}
+
+let store = createStore(counter);
+
+//display in console
+
+store.subscribe(() =>
+{
+  console.log(store.getState())
+})
+
+//dispatch
+
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
